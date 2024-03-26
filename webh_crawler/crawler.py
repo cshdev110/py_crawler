28c57e968d2391dd8c41da6ee56272d9e3f8a566
hostuser <kali@host.dev>
2024-03-26 23:15:09 +1000

    Extracting links from a website and completing the relative paths to full link.

--- webh_crawler/crawler.py
+++ webh_crawler/crawler.py
@@ -4,6 +4,9 @@
 ## Crawler
 
 import requests
+import re
+from urllib.parse import urljoin
+
 
 dmn = "hackthissite.org"
 
@@ -14,43 +17,60 @@ def request(uri):
         pass
 
 
+# Discover domain folders using wlist.txt
 def disc_domain():
     with open ("./subdomain_file/wlist.txt", "r") as wlist:
         itera = 0
         start = 0
         started_point = 15
         for line in wlist:
-                search = line.strip() + "." + dmn
+                search = line.strip() + "." + dmn  ##line.strip() to remove spaces
                 start += 1
                 if start >= started_point:
                     print(search)
                     if request(search):
                         print("[*] Discovered subdomain ==>> " + search)
-                    # itera += 1
+                    # itera += 1 ## For testing to make the for loop 50 iterations
                     # if itera == 50:
                     #     break
 
 
+# Discover files and folders using f_and_d_wlist.txt
 def disc_files_and_subdom():
      with open ("./subdomain_files/f_and_d_wlist.txt", "r") as wlist:
         itera = 0
         start = 0
         started_point = 0
         for line in wlist:
-                search = dmn + "/" + line.strip()
+                search = dmn + "/" + line.strip()  ##line.strip() to remove spaces
                 start += 1
                 if start >= started_point:
                     print(search)
                     if request(search):
                         print("[*] Discovered file or dir ==>> " + search)
-                    # itera += 1
+                    # itera += 1 ## For testing to make the for loop 50 iterations
                     # if itera == 50:
                     #     break
+                        
+
+def extract_uris(content_uri):
+    href_links = re.findall('(?:href=")(.*?)"', content_uri)
+    for uri_ in href_links:
+        # urljoin transform uri like /news/views/727 to a full uri https://www.hackthissite.org/news/views/727,
+        # and it leaves the other uri without modifications.
+        uri_ = urljoin("https://www." + dmn, uri_)
+        if (dmn in uri_):
+            print(uri_)
+     
 
 
 def spider(): 
     response = request(dmn)
-    print(response.content)
+    # .decode('utf-8', 'ignore') is to transfor byte to string as response.content retrives
+    # the website in byte format
+    # print(response.content.decode('utf-8', "ignore"))
+    extract_uris(response.content.decode('utf-8', "ignore"))
+
 
 
 # disc_domain()
